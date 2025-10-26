b5bd84ac9ac07d8434e5cacaef7fcba23c349df4
MichaelReeves2 <tadeas.jindra28@seznam.cz>
2025-10-26 14:07:26 +0100

    JS build prototype

--- dist/utils/scheduler.js
+++ dist/utils/scheduler.js
@@ -10,17 +10,25 @@ const constants_1 = require("./constants");
 const statisticsManager_1 = require("./statisticsManager");
 const startScheduler = () => {
     try {
-        //Every Monday midnight
-        node_cron_1.default.schedule("0 0 * * 1", () => {
-            (0, statisticsManager_1.showWeekStatistic)();
-            (0, statisticsManager_1.addWeeklySum)();
-            (0, statisticsManager_1.clearTimeValuesOfUsers)(constants_1.USER_TIMES_PATH);
-        });
-        //First of the month midnight
-        node_cron_1.default.schedule("0 0 1 * *", () => {
-            (0, statisticsManager_1.showMonthStatistic)();
-            (0, statisticsManager_1.clearTimeValuesOfUsers)(constants_1.USER_TIMES_PATH);
-            (0, statisticsManager_1.clearTimeValuesOfUsers)(constants_1.MONTH_TIMES_PATH);
+        node_cron_1.default.schedule("0 0 * * *", () => {
+            const now = new Date();
+            const dayOfWeek = now.getDay();
+            const dayOfMonth = now.getDate();
+            if (dayOfWeek == 1 && dayOfMonth == 1) {
+                (0, statisticsManager_1.showMonthStatistic)();
+                (0, statisticsManager_1.clearTimeValuesOfUsers)(constants_1.USER_TIMES_PATH);
+                (0, statisticsManager_1.clearTimeValuesOfUsers)(constants_1.MONTH_TIMES_PATH);
+            }
+            else if (dayOfWeek == 1) {
+                (0, statisticsManager_1.showWeekStatistic)();
+                (0, statisticsManager_1.addWeeklySum)();
+                (0, statisticsManager_1.clearTimeValuesOfUsers)(constants_1.USER_TIMES_PATH);
+            }
+            else if (dayOfMonth == 1) {
+                (0, statisticsManager_1.showMonthStatistic)();
+                (0, statisticsManager_1.clearTimeValuesOfUsers)(constants_1.USER_TIMES_PATH);
+                (0, statisticsManager_1.clearTimeValuesOfUsers)(constants_1.MONTH_TIMES_PATH);
+            }
         });
     }
     catch (error) {
