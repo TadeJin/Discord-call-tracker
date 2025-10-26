3452aabc03768ed4c642d0b8383481a8db66f995
MichaelReeves2 <tadeas.jindra28@seznam.cz>
2025-10-26 14:23:59 +0100

    fixed build

--- dist/utils/statisticsManager.js
+++ dist/utils/statisticsManager.js
@@ -135,7 +135,7 @@ const formatTimeData = (data) => {
         seconds = data;
     }
     if (hours == 0 && minutes == 0 && seconds == 0) {
-        return "NO_DATA";
+        return " NO_DATA";
     }
     else {
         return `${hours > 0 ? hours + " hours " : ""} ${minutes > 0 ? minutes + " minutes " : ""}${seconds > 0 ? seconds + " seconds" : ""}`;
